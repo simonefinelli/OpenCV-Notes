491018f0cabb6ed098f924936a6d8f94f0fd5480
Simone Finelli <simonefinelli.work@gmail.com>
2023-04-16 12:08:37 +0200

    Update read_image.py

---
+++ Basic/01_Reading-Saving Images and Videos/read_image.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+
+path_to_img = '../../assets/images/red_panda.jpg'
+
+# read an image from folder
+img = cv.imread(path_to_img)  # the image is read as numpy array
+
+# check the image shape
+shape = img.shape  # height, width, depth
+print(f'Height of the image: {shape[0]}')
+print(f'Width of the image: {shape[1]}')
+print(f'Depth of the image: {shape[2]}')
+
+# the cv image is a numpy array, so we can use np to performs operations
+print(np.shape(img))  # same result as above
+
+# display the image in a new window
+cv.imshow('Lesser Panda', img)  # name of window and matrix image
+cv.waitKey(0)  # to prevent the window closing
+               # 0 - indefinite waiting
+               # otherwise the windows is going to close after N milliseconds
