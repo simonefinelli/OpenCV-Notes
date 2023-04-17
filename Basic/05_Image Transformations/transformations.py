d0cb9806a4e9d946144f2394c9885ba946b45792
Simone Finelli <simonefinelli.work@gmail.com>
2023-04-17 22:40:13 +0200

    Update transformations.py

--- Basic/05_Image Transformations/transformations.py
+++ Basic/05_Image Transformations/transformations.py
@@ -1,19 +1,19 @@
 import cv2 as cv
 
-from utils import translate, rotate
+from utilities.utility import translate, rotate
 
 path_to_img = '../../assets/images/forest.jpg'
 img = cv.imread(path_to_img)  # 3 channel image (BGR)
 cv.imshow('Waterfall', img)
 
-# Translation
+# translation
 translated = translate(img, 100, 150)
 cv.imshow('Translated', translated)
 
 translated = translate(img, -100, -150)
 cv.imshow('Translated 2', translated)
 
-# Rotation
+# rotation
 rotated = rotate(img, 180)
 cv.imshow('Rotated', rotated)
 
@@ -21,8 +21,8 @@ rotation_point = (100, 300)
 rotated = rotate(img, -20, rotation_point)
 cv.imshow('Rotated 2', rotated)
 
-# Flipping
-flip_code = 0  # 0, 1, -1 -> vertically (x axis), horizontally (y axis), both
+# flipping
+flip_code = 1  # 0, 1, -1 -> vertically (x axis), horizontally (y axis), both
 flipped = cv.flip(img, flip_code)
 cv.imshow('Flipped', flipped)
 
@@ -30,5 +30,9 @@ flip_code = -1
 flipped = cv.flip(img, flip_code)
 cv.imshow('Flipped 2', flipped)
 
+# transposing
+transposed = cv.transpose(img)
+cv.imshow('Transposed', transposed)  # applying the transpose function twice,
+                                     # the original image is restored
 
 cv.waitKey(0)
