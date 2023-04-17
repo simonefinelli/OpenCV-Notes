28e51d601d2f705336d38e297c363c4fb02f1c7b
Simone Finelli <simonefinelli.work@gmail.com>
2023-04-17 21:59:12 +0200

    Update draw_text.py

--- Basic/04_Shapes and Text/draw_text.py
+++ Basic/04_Shapes and Text/draw_text.py
@@ -1,15 +1,25 @@
 import cv2 as cv
 import numpy as np
 
-# Text
-blank = np.zeros((500,500,3), dtype='uint8')
+# image
+blank = np.zeros((500, 500, 3), dtype='uint8')
+
+# text settings
 text = 'Hello World!'
 position = (150, 250)
-font = cv.FONT_HERSHEY_TRIPLEX
+# the font can we use are:
+# - FONT_HERSHEY_SIMPLEX, FONT_HERSHEY_PLAIN
+# - FONT_HERSHEY_DUPLEX, FONT_HERSHEY_COMPLEX
+# - FONT_HERSHEY_TRIPLEX, FONT_HERSHEY_COMPLEX_SMALL
+# - FONT_HERSHEY_SCRIPT_SIMPLEX, FONT_HERSHEY_SCRIPT_COMPLEX
+font = cv.FONT_HERSHEY_DUPLEX
 font_scale = 1.0
-color = (125, 0, 255)
+color = (0, 125, 255)
+
+# apply text
 cv.putText(blank, text, position, font, font_scale, color, thickness=2)
 
-cv.imshow('Text', blank)
+# show image
+cv.imshow('Custom Text', blank)
 
 cv.waitKey(0)
