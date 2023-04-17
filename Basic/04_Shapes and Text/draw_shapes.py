0528d62499740f9cc1460fa4fd4a5555e6ad6636
Simone Finelli <simonefinelli.work@gmail.com>
2023-04-17 21:41:56 +0200

    Update draw_shapes.py

--- Basic/04_Shapes and Text/draw_shapes.py
+++ Basic/04_Shapes and Text/draw_shapes.py
@@ -1,29 +1,29 @@
 import cv2 as cv
 import numpy as np
 
-# Black image
-blank = np.zeros((500,500,3), dtype='uint8')  # uint8 -> 8-bit unsigned (0-255)
+# black image
+blank = np.zeros((500, 500, 3), dtype='uint8')  # uint8 -> 8-bit unsigned (0-255)
 cv.imshow('Blank', blank)
 
-# Paint the whole image
-blank[:] = 0, 255, 0  # green - in cv2 channels are swapped (BGR)
+# paint the whole image
+blank[:] = 0, 255, 0  # green - (BGR)
 cv.imshow('Green', blank)
 
-# Square
-blank = np.zeros((500,500,3), dtype='uint8')
+# square
+blank = np.zeros((500, 500, 3), dtype='uint8')
 blank[100:200, 300:400] = 255, 255, 255  # white
 cv.imshow('Square', blank)
 
-# Rectangle
+# rectangle
 img = np.zeros((500,500,3), dtype='uint8')
 pt1 = (0,0)  # origin
 pt2 = (500, 250)
-color = (255, 0, 0)  # Blue - in cv2 channels are swapped (BGR)
+color = (255, 0, 0)  # Blue - (BGR)
 thickness = 2
 cv.rectangle(img, pt1, pt2, color, thickness=thickness)
 cv.imshow('Rectangle', img)
 
-# Filled Rectangle
+# filled Rectangle
 img = np.zeros((500,500,3), dtype='uint8')
 pt1 = (0,0)  # origin
 pt2 = (250, 250)
@@ -31,7 +31,7 @@ color = (0, 0, 255)  # Blue - in cv2 channels are swapped (BGR)
 cv.rectangle(img, pt1, pt2, color, thickness=cv.FILLED)  # or thickness=-1
 cv.imshow('Filled Rectangle', img)
 
-# Circle
+# circle
 img = np.zeros((500,500,3), dtype='uint8')
 center = (img.shape[1]//2, img.shape[0]//2)  # (x,y) -> width (shape[1]), height (shape[0])
 radius = 100  # pixels
@@ -40,7 +40,7 @@ thickness = 10
 cv.circle(img, center, radius, color, thickness=thickness)
 cv.imshow('Circle', img)
 
-# Line
+# line
 img = np.zeros((500,500,3), dtype='uint8')
 pt1 = (100, 250)  # origin
 pt2 = (400, 250)
@@ -48,12 +48,12 @@ color = (255, 255, 0)
 cv.line(img, pt1, pt2, color, thickness=5)
 cv.imshow('Line', img)
 
-# Polyline
+# polyline
 img = np.zeros((500,500,3), dtype='uint8')
 # points to draw
 pts = np.array([[249,100], [349,150], [299,250], [199,250], [149,150]], np.int32)
 # reshape our points (required by polylines)
-pts = pts.reshape((-1,1,2))
+pts = pts.reshape((-1, 1, 2))
 points = [pts]
 connected = True
 color = (255, 127, 127)
